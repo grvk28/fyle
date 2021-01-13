71527dd56be49efa2889ed4c3c15134d0b089061
grvk28 <40272398+grvk28@users.noreply.github.com>
2021-01-13 21:32:06 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (moreText.style.display === "none") {
+    moreText.style.display = "inline";
+    btnText.innerHTML = "Hide Categories"; 
+    
+  } else {
+    moreText.style.display = "none";
+    btnText.innerHTML = "See All Categories"; 
+    
+  }
+}
